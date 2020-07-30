d03d30e5b9900e94d5493a6e800a8159fe9cb49d
nao-3942 <68671778+nao-3942@users.noreply.github.com>
2020-07-30 17:44:01 +0900

    Delete final2.js

--- final/js/final2.js
+++
@@ -1,6 +0,0 @@
-myH = (new Date()).getHours();	
-if ( myH <= 5 ) myMes = "こんばんは~!";	
-else if ( myH <= 11 ) myMes = "おはようございます~!";	
-else if ( myH <= 18 ) myMes = "こんにちは~!";	
-else if ( myH <= 24 ) myMes = "こんばんは~!";	
-document.write( myMes );
